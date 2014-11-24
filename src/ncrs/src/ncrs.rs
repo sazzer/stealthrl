8efbdc70be6918ee248fb204a97124986b0009f0
Graham Cox <graham@grahamcox.co.uk>
2014-11-24 12:47:20 +0000

    Ability to enable/disable CBreak mode in a way that doesn't do any extra work

--- src/ncrs/src/ncrs.rs
+++ src/ncrs/src/ncrs.rs
@@ -2,7 +2,9 @@ extern crate ncurses;
 
 #[experimental]
 #[doc = "Data Type representing the NCurses system"]
-pub struct Ncrs;
+pub struct Ncrs {
+    cbreak: bool,
+}
 
 impl Drop for Ncrs {
     #[stable]
@@ -19,6 +21,24 @@ impl Ncrs {
     pub fn new() -> Ncrs {
         debug!("Creating a new NCRS");
         ncurses::initscr();
-        Ncrs
+        ncurses::nocbreak();
+        Ncrs {
+            cbreak: false
+        }
+    }
+    
+    #[experimental]
+    #[doc = "Set the CBreak mode to On or Off"]
+    pub fn cbreak(&mut self, on: bool) {
+        if self.cbreak != on {
+            if on {
+                debug!("Enabling CBreak mode");
+                ncurses::cbreak();
+            } else {
+                debug!("Disabling CBreak mode");
+                ncurses::nocbreak();
+            }
+            self.cbreak = on;
+        }
     }
 }
--- src/stealthrlui/src/lib.rs
+++ src/stealthrlui/src/lib.rs
@@ -1,5 +1,9 @@
 extern crate ncrs;
 
 pub fn stealthrlui() {
-	let ui = ncrs::Ncrs::new();
+	let mut ui = ncrs::Ncrs::new();
+	ui.cbreak(false);
+	ui.cbreak(true);
+	ui.cbreak(true);
+	ui.cbreak(false);
 }
